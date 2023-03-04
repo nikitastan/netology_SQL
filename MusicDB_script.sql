89cb23e7249866630d76879db4a46b4a091abbd9
nikitastan <118478764+nikitastan@users.noreply.github.com>
2023-03-04 11:37:40 +0300

    Update MusicDB_script.sql
    Изменение всех PK на тип данных SERIAL

--- MusicDB_script.sql
+++ MusicDB_script.sql
@@ -1,53 +1,53 @@
 create table if not exists musicians(
-	musician_id INTEGER PRIMARY KEY,
+	musician_id SERIAL PRIMARY KEY,
 	name VARCHAR(40) NOT NULL
 );
 
 create table if not exists albums(
-	album_id INTEGER PRIMARY KEY,
+	album_id SERIAL PRIMARY KEY,
 	name VARCHAR(40) NOT null,
 	year INTEGER not null
 );
 
 create table if not exists MusicianAlbum(
-	id INTEGER PRIMARY KEY,
+	id SERIAL PRIMARY KEY,
 	album_id INTEGER references albums(album_id),
 	musician_id INTEGER references musicians(musician_id)
 );
 
 create table if not exists Songs(
-	song_id INTEGER PRIMARY KEY,
+	song_id SERIAL PRIMARY KEY,
 	name VARCHAR(40) NOT null,
 	album_id INTEGER references albums(album_id),
 	duration TIME
 );
 
 create table if not exists Songs(
-	song_id INTEGER PRIMARY KEY,
+	song_id SERIAL PRIMARY KEY,
 	name VARCHAR(40) NOT null,
 	album_id INTEGER references albums(album_id),
 	duration TIME
 );
 
 create table if not exists Collections(
-	collection_id INTEGER PRIMARY KEY,
+	collection_id SERIAL PRIMARY KEY,
 	name VARCHAR(40) NOT null,
 	year INTEGER not null
 );
 
 create table if not exists SongsCollections(
-	id INTEGER PRIMARY KEY,
+	id SERIAL PRIMARY KEY,
 	song_id INTEGER references Songs(song_id),
 	collection_id INTEGER references Collections(collection_id)
 );
 
 create table if not exists Genres(
-	genre_id INTEGER PRIMARY KEY,
+	genre_id SERIAL PRIMARY KEY,
 	name VARCHAR(40) NOT NULL
 );
 
 create table if not exists MusicianGenre(
-	id INTEGER PRIMARY KEY,
+	id SERIAL PRIMARY KEY,
 	genre_id INTEGER references Genres(genre_id),
 	musician_id INTEGER references musicians(musician_id)
 );
